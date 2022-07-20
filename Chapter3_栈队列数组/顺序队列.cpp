166e3261304a7fb9721177f68e52605212b2cf61
Dy-6114 <1980506537@qq.com>
2022-07-20 22:18:23 +0800

    更新

--- Chapter3_栈队列数组/顺序队列.cpp
+++ Chapter3_栈队列数组/顺序队列.cpp
@@ -18,6 +18,29 @@ bool QueueEmpty(SqQueue Q){
     else return false;
 }
 
+//入队
+bool EnQueue(SqQueue &Q, int x){
+    if ((Q.rear + 1) % MaxSize == Q.front) return false;
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+    
+}
+
+//出队
+bool DeQueue(SqQueue &Q, int &x){
+    if (Q.rear == Q.front) return false;
+    x = Q.data[Q.front];
+    Q.front = (Q.front + 1) % MaxSize;
+    return true;
+}
+
+//获得队头元素
+bool GetHead(SqQueue &Q, int &x){
+    if (Q.rear == Q.front) return false;
+    x = Q.data[Q.front];
+    return true;
+}
 
 
 

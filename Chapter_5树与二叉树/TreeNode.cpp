6d62e287c889c07756c033381db006dc06aa2f76
Dy-6114 <1980506537@qq.com>
2022-07-26 21:01:19 +0800

    树与二叉树

---
+++ Chapter_5树与二叉树/TreeNode.cpp
@@ -0,0 +1,20 @@
+//顺序存储：一定要把二叉树的节点编号与完全二叉树对应起来
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxSize 100
+
+struct TreeNode
+{
+    int value;
+    bool isEmpty;
+};
+
+TreeNode t[MaxSize]; //t[0]不存结点
+
+int main()
+{
+    for (int i = 0; i < MaxSize; i++)
+    {
+        t[i].isEmpty = true;
+    }
+}

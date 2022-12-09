850db8c267d751b6cada426fb84d027c0bde553a
ivandong <dongivan6114@gmail.com>
2022-12-09 10:20:13 +0800

    大根堆（升序排序）

--- Chapter8_排序/堆排序.cpp
+++ Chapter8_排序/堆排序.cpp
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-void Swap(ElemType a,ElemType b);//输出堆顶元素（和堆底元素交换）
+typedef int ElemType;
 
-void BuildMaxHeap(ElemType A[],int len){
-    for(int i = len/2;i>0;i--)
-        HeadAdjust(A,i,len);
-}
-
-void HeadAdjust(ElemType A[],int k,int len){
-    A[0] = A[k];
+void HeadAdjust(ElemType A[],int k,int len){ //堆调整
+    A[0] = A[k]; //A[0]作为哨兵
     for (int i = 2*k; i <=len; i*=2)
     {
         if(i<len&&A[i]<A[i+1])
@@ -23,12 +18,28 @@ void HeadAdjust(ElemType A[],int k,int len){
     A[k] = A[0];
 }
 
+void BuildMaxHeap(ElemType A[],int len){
+    for(int i = len/2;i>0;i--)
+        HeadAdjust(A,i,len);
+}
+
 void HeapSort(ElemType A[],int len){
     BuildMaxHeap(A,len);
     for (int  i = len; i > 1; i--)
     {
-        Swap(A[i],A[1]);
+        swap(A[i],A[1]);
         HeadAdjust(A,1,i-1);
     }
     
+}
+
+int main(){
+    ElemType A[] = {-10000,1,9,7,11,5,6,8};
+    HeapSort(A,7);
+    cout<<endl;
+    for (int i = 0; i < 8; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    return 0;
 }

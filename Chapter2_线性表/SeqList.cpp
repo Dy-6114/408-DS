d056c3615057f7f18f7af176d1e7c482bb6a23a5
Ivan40n9 <62270797+Dy-6114@users.noreply.github.com>
2023-03-10 16:02:35 +0800

    Update SeqList.cpp

--- Chapter2_线性表/SeqList.cpp
+++ Chapter2_线性表/SeqList.cpp
@@ -16,6 +16,16 @@ void InitList(SeqList &L)
 	L.MaxSize = InitSize;
 }
 
+void IncreaseSize(SeqList &L, int len){
+	int* p = L.data;
+	L.data = (int *)malloc((L.MaxSize + len)*sizeof(int));
+	for(int i = 0; i < L.length; i++){
+		L.data[i] = p[i];
+	}
+	L.MaxSize += len;
+	free(p);
+}
+
 bool ListInsert(SeqList &L, int i, int e)
 {
 	if (i < 1 || i > L.length + 1)

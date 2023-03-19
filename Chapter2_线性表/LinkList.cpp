489f9f37257e6db2be1ce7175b901482aa0487c8
Ivan40n9 <62270797+Dy-6114@users.noreply.github.com>
2023-03-19 15:45:52 +0800

    Update LinkList.cpp

--- Chapter2_线性表/LinkList.cpp
+++ Chapter2_线性表/LinkList.cpp
@@ -71,12 +71,10 @@ bool Empty(LinkList L)
 //按位查找，返回第i个元素（带头结点）
 LNode *GetElem(LinkList L, int i)
 {
-	int j = 1;
-	LNode *p = L->next;
-	if (i == 0)
-		return L;
-	if (i < 1)
+	if (i < 0)
 		return NULL;
+	int j = 0;
+	LNode *p = L;
 	while (p != NULL && j < i)
 	{
 		p = p->next;

bb3315b6d1f9bff8f57afa296be4ae1ac94f4fb3
Ivan40n9 <62270797+Dy-6114@users.noreply.github.com>
2023-12-06 16:33:54 +0800

    Update 堆排序.cpp

--- Chapter8_Sort/堆排序.cpp
+++ Chapter8_Sort/堆排序.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 typedef int ElemType;
 
-void HeadAdjust(ElemType A[],int k,int len){ //堆调整
-    A[0] = A[k]; //A[0]作为哨兵
+void HeadAdjust(ElemType A[],int k,int len){ //将元素k为根的子树进行堆调整
+    A[0] = A[k]; //A[0]作为哨兵，暂存子树的根结点
     for (int i = 2*k; i <=len; i*=2)
     {
-        if(i<len&&A[i]<A[i+1])
+        if(i<len&&A[i]<A[i+1]) //无右孩子或选出左右孩子中的最大孩子
             i++;
-        if(A[0]>=A[i]) break;
+        if(A[0]>=A[i]) break; //根结点大于最大孩子
         else{
-            A[k] = A[i];
+            A[k] = A[i];  //继续向下调整
             k = i;
         }
     }
-    A[k] = A[0];
+    A[k] = A[0];  //一轮调整后子树根结点的最终位置
 }
 
-void BuildMaxHeap(ElemType A[],int len){
+void BuildMaxHeap(ElemType A[],int len){  //大根堆
     for(int i = len/2;i>0;i--)
         HeadAdjust(A,i,len);
 }
@@ -27,7 +27,7 @@ void HeapSort(ElemType A[],int len){
     BuildMaxHeap(A,len);
     for (int  i = len; i > 1; i--)
     {
-        swap(A[i],A[1]);
+        swap(A[i],A[1]);  //交换值
         HeadAdjust(A,1,i-1);
     }
     
@@ -37,7 +37,7 @@ int main(){
     ElemType A[] = {-10000,1,9,7,11,5,6,8};
     HeapSort(A,7);
     cout<<endl;
-    for (int i = 0; i < 8; i++)
+    for (int i = 1; i < 8; i++)
     {
         cout<<A[i]<<" ";
     }
